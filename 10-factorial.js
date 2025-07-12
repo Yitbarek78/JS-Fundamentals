067407f074201dcba50ba3f39c2dc292130eaffb
yitbarek <yitbarekwelde658@gmail.com>
2025-07-12 15:36:16 +0300

    Factorial calculator file created

---
+++ 10-factorial.js
@@ -0,0 +1,10 @@
+const args = process.argv.slice(2);
+const num = Number(args[0]);
+
+function factorial(n) {
+  if (isNaN(n)) return 1;
+  if (n <= 1) return 1;
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(num));
